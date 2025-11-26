c1f05c726507bab2baef1ffa1918a2d9031070a9
Panigala <laggerav@outlook.com>
2025-11-26 15:57:25 +0530

    Update future_pdf_introduction_scrapper_version1.py

--- src/scraper/future_pdf_introduction_scrapper_version1.py
+++ src/scraper/future_pdf_introduction_scrapper_version1.py
@@ -20,7 +20,6 @@ def extract_text_by_columns(pdf_path):
         return full_text
 
 def extract_introduction_from_pdf(pdf_path):
-    
     full_text = extract_text_by_columns(pdf_path) #extract text from entire file
     intro_regex = r"(introduction|\d+\.\s*introduction|\bINTRODUCTION\b)" #regular expression for matching
     match = re.search(intro_regex, full_text, re.IGNORECASE)
@@ -29,30 +28,19 @@ def extract_introduction_from_pdf(pdf_path):
     else:
         return None  # no intro at all
     text_from_intro = full_text[start_index:]
-    
-    heading_regex = r"\n([A-Z][A-Z0-9\s\.\-]+)(?=\n|\s|$)"
-    
+    #heading_regex = r"\n([A-Z][A-Z0-9\s\.\-]+)(?=\n|\s|$)"# for possible heading 
     end_index = text_from_intro.lower().find("Background")  # like next tilte name
-
     if end_index != -1:
         text_from_intro = text_from_intro[:end_index]
-    
     return text_from_intro
-
-
-
 def process_pdfs_in_folder(pdf_folder):#whatever in folder it will be proccessd
     output_folder = "extracted_introductions" #results
     os.makedirs(output_folder, exist_ok=True)
-
     pdf_files = [f for f in os.listdir(pdf_folder) if f.endswith('.pdf')]
-    
     for pdf_file in pdf_files:
         pdf_path = os.path.join(pdf_folder, pdf_file)
-        
         try:
             intro_text = extract_introduction_from_pdf(pdf_path) #extract intro
-            
             if intro_text:
                 output_file = os.path.join(output_folder, f"{os.path.splitext(pdf_file)[0]}_introduction.txt") #save the extracted intro in per file .txt here
                 with open(output_file, 'w', encoding='utf-8') as f:
@@ -62,7 +50,6 @@ def process_pdfs_in_folder(pdf_folder):#whatever in folder it will be proccessd
                 print(f"Introduction not found in {pdf_file}")
         except Exception as e:
             print(f"Error processing {pdf_file}: {e}")
-
 #main
 pdf_folder = 'pdf'  #raw pdf folder
 process_pdfs_in_folder(pdf_folder)
