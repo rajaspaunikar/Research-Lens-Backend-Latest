34dbbc9a8080e3f55f581f2ab813db163810426e
Shivraj1906 <shivrajnaghera@outlook.com>
2025-11-28 11:42:27 +0530

    api changes

--- src/api.py
+++ src/api.py
@@ -80,6 +80,14 @@ def stats():
 
         return [
             {
+                "label": "RECENT INFLUX",
+                "value": f"+{recent}",
+                "description": "LAST 7 DAYS",
+                "intent": "neutral",
+                "icon": "activity",
+                "tag": "Active"
+            },
+            {
                 "label": "PAPERS PARSED",
                 "value": f"{cnt}",
                 "description": "TOTAL DATABASE",
@@ -95,14 +103,6 @@ def stats():
                 "icon": "star",
                 "direction": "up"
             },
-            {
-                "label": "RECENT INFLUX",
-                "value": f"+{recent}",
-                "description": "LAST 7 DAYS",
-                "intent": "neutral",
-                "icon": "activity",
-                "tag": "Active ⚡"
-            }
         ]
     finally:
         s.close()
